2de6ea98be5d3241aa61f9d79d0823be2819359f
Ronald Kolum <kolumronnie@gmail.com>
2022-03-17 09:58:09 +0300

    added News logic

--- src/main/java/models/pojos/News.java
+++ src/main/java/models/pojos/News.java
@@ -1,4 +1,61 @@
 package models.pojos;
 
+import java.util.Objects;
+
 public class News {
+    private int newsId;
+    private String headline;
+    private String content;
+    private int department_id;
+
+    public News(String headline, String content, int department_id) {
+        this.headline = headline;
+        this.content = content;
+        this.department_id = department_id;
+    }
+
+    public int getNewsId() {
+        return newsId;
+    }
+
+    public void setNewsId(int newsId) {
+        this.newsId = newsId;
+    }
+
+    public String getHeadline() {
+        return headline;
+    }
+
+    public void setHeadline(String headline) {
+        this.headline = headline;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getDepartment_id() {
+        return department_id;
+    }
+
+    public void setDepartment_id(int department_id) {
+        this.department_id = department_id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        News news = (News) o;
+        return newsId == news.newsId && department_id == news.department_id && Objects.equals(headline, news.headline) && Objects.equals(content, news.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(newsId, headline, content, department_id);
+    }
 }
