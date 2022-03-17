1e001aad2cc967b9b50c20ed551e2f14e4581eb1
Ronald Kolum <kolumronnie@gmail.com>
2022-03-17 09:50:52 +0300

    added gitignore and dependancies

--- src/main/java/models/pojos/Users.java
+++ src/main/java/models/pojos/Users.java
@@ -1,5 +1,9 @@
 package models.pojos;
 
 public class Users {
-
+    private int id;
+    private String name;
+    private String position;
+    private String role;
+    private int department_id;
 }
